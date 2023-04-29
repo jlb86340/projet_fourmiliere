dc79e581de1b4934529970749afea72189a869a4
mathieu <math86340@orange.fr>
2023-04-29 22:02:25 +0200

    vive la couleur

--- src/main/java/com/example/projet_fourmiliere/jeuDesFourmis/Main.java
+++ src/main/java/com/example/projet_fourmiliere/jeuDesFourmis/Main.java
@@ -1,14 +1,13 @@
 package com.example.projet_fourmiliere.jeuDesFourmis;
 
 import com.example.projet_fourmiliere.jeuDesFourmis.controller.Controller;
-import com.example.projet_fourmiliere.jeuDesFourmis.model.Fourmi;
 import com.example.projet_fourmiliere.jeuDesFourmis.model.Fourmiliere;
 import com.example.projet_fourmiliere.jeuDesFourmis.vue.Interface;
 import javafx.application.Application;
 import javafx.scene.Scene;
 import javafx.stage.Stage;
 
-public class test extends Application {
+public class Main extends Application {
     @Override
     public void start(Stage stage) throws Exception {
 
